4a1362727755e0872efeaf197b61bff4bf4479b9
KGrim23 <kalaya.grimshaw@gmail.com>
2024-08-24 14:28:56 +0100

    Initial Commit

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,32 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+import NavBar from "@/components/Navbar";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* LEFT*/}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2 p-4"
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">The Grimshaw</span>
+        </Link>
+        <Menu />
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:-[92%] lg:-[84%] xl:-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
