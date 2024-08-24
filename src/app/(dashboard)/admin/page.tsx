7407b65b6a37cd74f8f56d3c6f8ea4f68d53f00b
KGrim23 <kalaya.grimshaw@gmail.com>
2024-08-24 15:20:14 +0100

    wip saved.
    - Done Nabar
    - Logo and Title
    - Done User Cards
    - Menu.tsx done for map funtions

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,23 @@
+import UserCard from "@/components/UserCard";
+
 const AdminPage = () => {
-  return <div>Admin Page</div>;
+  return (
+    <div className="p-4 flex flex-col gap-4 md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="Staff" />
+          <UserCard type="Teacher" />
+          <UserCard type="Student" />
+          <UserCard type="Parent" />
+        </div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3"></div>
+    </div>
+  );
 };
 
 export default AdminPage;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,11 @@ const Navbar = () => {
       {/* SEARCH BAR */}
       <div className="hidden md:flex items-center gap-2 text-xs rounded-full ring-[1.5px] ring-gray-300 px-2">
         <Image src="/search.png" alt="search" width={14} height={14} />
-        <input type="text" placeholder="Search..." className="" />
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-[200px] h-[30px] bg-transparent outline-none"
+        />
       </div>
 
       {/* ICON AND USER */}
