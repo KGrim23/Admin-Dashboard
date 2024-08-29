2c8b3e120c92ee3a46fa24a157adf10fd55a142a
KGrim23 <kalaya.grimshaw@gmail.com>
2024-08-29 20:02:04 +0100

    create Classes Page

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -3,25 +3,38 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, subjectsData } from "@/lib/data";
+import { role, parentsData, classesData } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-type Subject = {
+type Class = {
   id: number;
   name: string;
-  teachers: string[];
+  capacity: string;
+  grade: string;
+  supervisor: string[];
 };
 
+// HEADINGS
 const columns = [
   {
     header: "Subject Name",
-    accessor: "info",
+    accessor: "Subject Name",
   },
   {
-    header: "Teachers",
-    accessor: "teachers",
+    header: "Capacity",
+    accessor: "Capacity",
     className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Supervisor",
+    accessor: "supervisor",
+    className: "hidden lg:table-cell",
   },
   {
     header: "Actions",
@@ -29,8 +42,8 @@ const columns = [
   },
 ];
 
-const SubjectPage = () => {
-  const renderRow = (item: Subject) => (
+const ParentListPage = () => {
+  const renderRow = (item: Class) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-gray-50 text-sm hover:bg-lamaPurpleLight"
@@ -40,7 +53,9 @@ const SubjectPage = () => {
           <h3 className="font-semibold">{item.name}</h3>
         </div>
       </td>
-      <td className="hidden md:table-cell">{item.teachers.join(", ")}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.capacity}</td>
+      <td className="hidden md:table-cell">{item.supervisor}</td>
 
       <td>
         <div className="flex items-center gap-2">
@@ -60,7 +75,7 @@ const SubjectPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP HEADINGS */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Subjects</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -78,7 +93,7 @@ const SubjectPage = () => {
       </div>
       {/* LIST */}
       <div className="flex flex-col">
-        <Table columns={columns} renderRow={renderRow} data={subjectsData} />
+        <Table columns={columns} renderRow={renderRow} data={classesData} />
       </div>
 
       {/* Pagination - page numbers */}
@@ -87,4 +102,4 @@ const SubjectPage = () => {
   );
 };
 
-export default SubjectPage;
+export default ParentListPage;
